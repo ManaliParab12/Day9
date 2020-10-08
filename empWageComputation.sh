5f1734288517b0adfd903fdbb61559f97218f259
ManaliParab12 <manaliparab10@gmail.com>
2020-10-09 01:35:20 +0530

    Calculated Total Wages for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -13,7 +13,12 @@ fi
 
 isPartTime=1
 isFullTime=2
+monthlyWage=0
 empRatePerHr=20
+numWorkingDays=20
+
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
 
 empCheck=$((RANDOM%3))
 
@@ -30,4 +35,5 @@ case $empCheck in
 esac
 
 dailyWage=$(($empHrs*$empRatePerHr))
-
+monthlyWage=$(($monthlyWage+$dailyWage))
+done
