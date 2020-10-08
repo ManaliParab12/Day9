d4323c95dc6e8a6e579f654d7421ccee9741a6eb
ManaliParab12 <manaliparab10@gmail.com>
2020-10-09 02:11:35 +0530

    Stored Day Wage and Daily Wage along with the Total Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -20,33 +20,39 @@ numWorkingDays=20
 totalWorkHours=0
 totalWorkingDays=0
 
-function getWorkingHours() {
+declare -A dailyWage
 
-         case $1 in
+function getWorkingHours() {
+         local $empcheck=$1
+         case $empCheck in
                   $isFullTime)
-                        empHrs=8;
+                        workHrs=8;
                         ;;
                   $isPartTime)
-                        empHrs=4;
+                        workHrs=4;
                         ;;
                   *)
-                        empHrs=0;
+                        workHrs=0;
                         ;;
          esac
+         echo $workHrs
 }
-
 
 function getEmpWage() {
-        echo $(($1*$empRatePerHr))
+        local empHr=$1
+        echo $(($empHr*empRatePerHr))
 }
-
 
 while [[ $totalWorkHours -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
 do
         ((totalWorkingDays++))
-        getWorkingHours $((RANDOM%3))
-        totalWorkHours=$(($totalWorkHours+$empHrs))
-        dailyWages[$totalWorkingDays]=$(($empHrs*$empRatePerHr)) 
+        empCheck=$((RANDOM%3))
+        workHrs="$( getWorkingHours $empCheck )"
+        totalWorkHours=$(($totalWorkHours+$workHrs))
+        dailyWage["Day" $totalWorkingDays]="$( getEmpWage $workHrs )"
 done
 
-totalSalary="$( getEmpWage $totalWorkHours )"
+totalSalary=$(($totalWorkHours*$empRatePerhr))
+echo ${dailywage[@]}
+echo ${!dailyWage[@]}
+
