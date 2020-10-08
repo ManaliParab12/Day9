f1fd73833182a4d1f2efb4472201c6719da52e4b
ManaliParab12 <manaliparab10@gmail.com>
2020-10-09 01:25:59 +0530

    Solved using Switch Case Statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,18 +11,23 @@ else
     echo "Employee is Absent"
 fi
 
-isPartTime=0
-isFullTime=1
+isPartTime=1
+isFullTime=2
 empRatePerHr=20
 
-empCheck=$((RANDOM%2))
+empCheck=$((RANDOM%3))
 
-if [ $isFullTime -eq $empCheck ]
-then
-    empHrs=8;
-else
-    empHrs=4;
-fi
+case $empCheck in
+       $isFullTime)
+              empHrs=8;
+              ;;
+       $isPartTime)
+              empHrs=4;
+              ;;
+       *)
+              empHrs=0;
+              ;;
+esac
 
 dailyWage=$(($empHrs*$empRatePerHr))
 
